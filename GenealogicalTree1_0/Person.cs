6fcaa90b1f99976e00fe3f324a5910ff3f00e8f0
VitaliiOlefirenko <84074553+VitaliiOlefirenko@users.noreply.github.com>
2021-05-18 09:35:36 +0300

    HomeWork
    HomeWork

---
+++ GenealogicalTree1_0/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace GenealogicalTree
+{
+    public class Person
+    {
+        public List<Person> Children { get; set; }
+        public readonly Gender gender;
+        public string FirstName { get; }
+        public string LastName { get; }
+        public DateTime Birthday { get; }
+        public int Age
+        {
+            get
+            {
+                var now = DateTime.Today;
+
+                return now.Year - Birthday.Year;
+            }
+        }
+
+        public Person(string firstName, string lastName, DateTime birthday, Gender gender, List<Person> children = null)
+        {
+            this.FirstName = firstName;
+            LastName = lastName;
+            Birthday = birthday;
+            this.gender = gender;
+            Children = children;
+        }
+
+        public Person()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{ConvertGenderToText()} {FirstName} {LastName} was born in {Birthday.ToString("dd/MMM/yyyy", new CultureInfo("en-GB"))}";
+        }
+
+        private string ConvertGenderToText()
+        {
+            return gender == Gender.Male ? "Mister." : "Miss.";
+        }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ VariablesDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace VariablesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("About which language do you want to know information? 1 - English, 2 - Spanish, 3 - Ukrainian ");
+            string line = Console.ReadLine();
+            byte languageValue;
+            string errorMessage = "Please follow the instructions and try again";
+            if (!byte.TryParse(line, out languageValue))
+            {
+                Console.Write(errorMessage);
+            }
+            else
+            {
+                if (languageValue.Equals(1) || languageValue.Equals(2) || languageValue.Equals(3))
+                {
+                    Console.WriteLine($"With 2,500 to 3,000 words, you can understand 90% of everyday {(Language)languageValue} conversations,");
+                    Console.WriteLine($"{(Language)languageValue} newspaper and magazine articles, and {(Language)languageValue} used in the workplace.");
+                    Console.WriteLine($"So it is essential to learn the right {(Language)languageValue} vocabulary words.");
+                }
+                else
+                {
+                    Console.Write(errorMessage);
+                }
+            }
+        }
+
+        public enum Language : byte
+        {
+            English = 1,
+            Spanish = 2,
+            Ukrainian = 3
+        }
+    }
+}
